03766425ba48810ac6f1cc7421a3ba93f99deb0b
Myckhel <myckhel1@hotmail.com>
2020-07-07 23:37:03 +0100

    used vtpass class for tv verification

--- app/Http/Controllers/TvSubscriptionController.php
+++ app/Http/Controllers/TvSubscriptionController.php
@@ -3,125 +3,38 @@
 namespace App\Http\Controllers;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Vtpass;
 
 class TvSubscriptionController extends Controller
 {
 
   public function verify_gotv_card_number(Request $request)
   {
-    $username = "enluxtech@gmail.com"; //email address(sandbox@vtpass.com)
-    $password = "Comkid@1"; //password (sandbox)
-    $host = 'https://sandbox.vtpass.com/api/merchant-verify';
-    $data = array(
-      'serviceID'=> $request->serviceID, //integer e.g gotv,dstv,eko-electric,abuja-electric
-      'billersCode'=> $request->billersCode, // e.g smartcardNumber, meterNumber,
-    );
-    $curl       = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => $host,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_USERPWD => $username.":" .$password,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $data,
-    ));
-    $response = json_decode(curl_exec( $curl ),true);
-  $err = curl_error($curl);
+    $data = [
+      'serviceID'   => $request->serviceID, //integer e.g gotv,dstv,eko-electric,abuja-electric
+      'billersCode' => $request->billersCode, // e.g smartcardNumber, meterNumber,
+    ];
 
-  curl_close($curl);
-
-  if ($err) {
-    return response()->json([
-   'code' => '404',
-   'data'  => "cURL Error #:" . $err,
-], 404);
-  } else {
-
-    return $response;
+    return Vtpass::verify($data);
   }
-
-
-    }
     public function verify_startime_card_number(Request $request)
     {
-      $username = "enluxtech@gmail.com"; //email address(sandbox@vtpass.com)
-      $password = "Comkid@1"; //password (sandbox)
-      $host = 'https://sandbox.vtpass.com/api/merchant-verify';
       $data = array(
         'serviceID'=> $request->serviceID, //integer e.g gotv,dstv,eko-electric,abuja-electric
         'billersCode'=> $request->billersCode, // e.g smartcardNumber, meterNumber,
       );
-      $curl       = curl_init();
-      curl_setopt_array($curl, array(
-      CURLOPT_URL => $host,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_USERPWD => $username.":" .$password,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => $data,
-      ));
-      $response = json_decode(curl_exec( $curl ),true);
-    $err = curl_error($curl);
 
-    curl_close($curl);
-
-    if ($err) {
-      return response()->json([
-     'code' => '404',
-     'data'  => "cURL Error #:" . $err,
-  ], 404);
-    } else {
-
-      return $response;
+      return Vtpass::verify($data);
     }
-
-
-      }
 
 
   public function verify_dstv_card_number(Request $request)
   {
-    $username = "enluxtech@gmail.com"; //email address(sandbox@vtpass.com)
-    $password = "Comkid@1"; //password (sandbox)
-    $host = 'https://sandbox.vtpass.com/api/merchant-verify';
     $data = array(
       'serviceID'=> $request->serviceID, //integer e.g gotv,dstv,eko-electric,abuja-electric
       'billersCode'=> $request->billersCode, // e.g smartcardNumber, meterNumber,
     );
-    $curl       = curl_init();
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => $host,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_USERPWD => $username.":" .$password,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $data,
-    ));
-    $response = json_decode(curl_exec( $curl ),true);
-  $err = curl_error($curl);
-
-  curl_close($curl);
-
-  if ($err) {
-    return response()->json([
-   'code' => '404',
-   'data'  => "cURL Error #:" . $err,
-], 404);
-  } else {
-
-    return $response;
-  }
-
-
+    return Vtpass::verify($data);
     }
 
     public function startime_sub_vtu_api(Request $request)
